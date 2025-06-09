2571563e3db260b74da35b6c4c61cbb72a8443df
Rara Soro <raras@spc.int>
2025-06-10 09:50:39 +1100

    Added the IMTS table 1 processing

--- IMTSprocessing.R
+++ IMTSprocessing.R
@@ -7,6 +7,35 @@ library(data.table)
 library(openxlsx)
 library(readxl)
 library(dplyr)
+library(tidyverse)
+
+
+#### *********************** IMTS table 1 processing *********************************** ####
+
+table1 <- read_excel("data/imtsData.xlsx", sheet = "table1")
+
+table1 <- table1 %>%
+  pivot_longer(
+    cols = c(X, M, TB),
+    names_to = "INDICATOR",
+    values_to = "OBS_VALUE"
+  )
+
+table1b <- read_excel("data/imtsData.xlsx", sheet = "table1b")
+
+table1b <- table1b %>%
+  pivot_longer(
+    cols = c(X, M, TB),
+    names_to = "INDICATOR",
+    values_to = "OBS_VALUE"
+  )
+
+table1_final <- rbind(table1, table1b)
+
+#Output table in csv format
+write.csv(table1_final, "output/imts/DF_IMTS_TABLE1.csv", row.names = FALSE)
+
+
 
 #### ********************** IMTS table 2 processing *********************************** #### 
 
